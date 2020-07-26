91dfdf4510680f2ebbc57611751b7750db3c2581
amartulse <nagarkar.amar@gmail.com>
2020-07-26 14:42:13 +0000

    2nd Commit

---
+++ practise/src/TestInheritance/CreateNewLoan.java
@@ -0,0 +1,14 @@
+package TestInheritance;
+import java.util.Scanner;
+
+public class CreateNewLoan {
+	
+	public static void main (String args[]) {
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("Enter Type of Loan(1 for Home | 2 for Car):");
+		String opt = s1.nextLine();
+		if (opt.matches("1")) {
+			Home
+		}
+	}
+	
---
+++ practise/src/TestInheritance/LoanProcess.java
@@ -0,0 +1,24 @@
+package TestInheritance;
+
+import java.util.Random;
+
+public class LoanProcess {
+	protected long customerid;
+	protected String customername;
+	protected double loanamount;
+	protected int term;
+	protected static double interest = 8.5;
+	
+	public String getCustomername() {
+		return customername;
+		
+	}
+	
+	LoanProcess() {
+		customerid = new Random().nextLong();
+			}
+	public double calculateEMI() {
+		return (loanamount*interest/100)/term;
+	}
+
+}
